bf639804a929d5ea9c49c1d15f258d63738813fd
Lautaro Famaf <lautaro.deco@mi.unc.edu.ar>
2025-05-02 04:54:06 -0300

    more comments

--- Queue.cc
+++ Queue.cc
@@ -53,27 +53,54 @@ void Queue::handleMessage(cMessage *msg) {
         // if packet in buffer, send next one
         if (!buffer.isEmpty()) {
             // dequeue packet
+            // -> [p1,p2,p3] ->
             cPacket *pkt = (cPacket*) buffer.pop();
-            // send packets
+            // -> [p1,p2,  ] ->
+            //           p3 <- pkt
+            // send packet
             send(pkt, "out");
-            // start new service
-            serviceTime = pkt->getDuration();
-            scheduleAt(simTime() + serviceTime, endServiceEvent);
+            // -> [p1,p2, ] -> p3
+            //                 ^-- pkt.getDuration
+
+            // triggereo el evento endServiceEvent para auto-avisarme que el paquete popeado ha sido
+            // entregado (finalizo su viaje por el medio), cuando llega sigo enviando paquetes
+            serviceTime = pkt->getDuration();                     // <- serviceTime: En cuanto tiempo se mandara
+                                                                  //                 el paquete por el medio
+                                                                  // Omnet ya sabe que el paquete tardara
+                                                                  // getDuration() = Tamano / Datarate_canal
+                                                                  // en viajar por el medio
+            scheduleAt(simTime() + serviceTime, endServiceEvent); // <- scheduleAt(<pkt_llegada_momento>, endServiceEvent):
+                                                                  // Agendo el evento al momento en el que llegara el pkt
+                                                                  // pkt_llegada_momento = simTime() + serviceTime
+            //                            _______
+            //                           |       | <-- Es un canal out con un datarate D
+            // Tx -> p3 -> Queue: [p1p2] ->    ->
+            //                               ^-- pkt
+            //                               pkt.getDuration = D/pkt.Size
         }
+        /*
+        pkt sale de Tx y comienza su viaje por el medio
+                               (*)          (**)
+        Tx -> <     > -> Queue ->            -> Rx
+                                ^-- T (marco el tiempo actual T == simTime())
+            (*):
+                - Apenas sale hago send(out,pkt)
+                - D = pkt.getDuration = pkt.Size / out.Datarate
+                - Scheduleo el endServiceEvent en T + D = (**)
+        */
     } else { // if msg is a data packet
-        // enqueue the packet
         if(buffer.getLength() >= par("bufferSize").intValue()){
+            //drop the packet
             delete msg;
             this->bubble("packet dropped");
             packetDropVector.record(1);
             packetDrop++;
-
-
         }else{
-        buffer.insert(msg);
-        bufferSizeVector.record(buffer.getLength());
-        packetEnqueued++;
-        // if the server is idle
+            // enqueue the packet
+            buffer.insert(msg);
+            bufferSizeVector.record(buffer.getLength());
+            packetEnqueued++;
+            // if the server is idle
             if (!endServiceEvent->isScheduled()) {
                 // start the service
                 scheduleAt(simTime() + 0, endServiceEvent);
